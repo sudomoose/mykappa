03dafaee2a51b939731be775470bbcf32065a52a
moosecoop <cooper.makhijani@icloud.com>
2017-11-05 17:16:05 -0800

    sthings

--- src/base/Base.js
+++ src/base/Base.js
@@ -5,6 +5,9 @@ function Kappa(n) {
   this.age = 0;
   this.type = 'default';
   this.timeTypeSet = 0;
+  this.picture = "http://i3.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large";
+  this.types = ["Kappa", "KappaPride", "KappaRoss", "KappaClaus", "KappaWealth"];
+  this.maxAvailableType = 0;
 }
 
 Kappa.prototype.setType = function (type) {

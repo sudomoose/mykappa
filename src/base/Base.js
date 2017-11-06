336f4cc55bdb32dba9357b106d2c54936960cc5e
moosecoop <cooper.makhijani@icloud.com>
2017-11-05 17:03:28 -0800

    initial commit

---
+++ src/base/Base.js
@@ -0,0 +1,12 @@
+module.exports = Kappa;
+
+function Kappa(n) {
+  this.name = n;
+  this.age = 0;
+  this.type = 'default';
+  this.timeTypeSet = 0;
+}
+
+Kappa.prototype.setType = function (type) {
+
+};

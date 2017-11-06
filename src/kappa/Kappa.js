d99c8f5d6a51086efd8c7239c674a0a3256d1f54
moosecoop <cooper.makhijani@icloud.com>
2017-11-05 17:58:39 -0800

    SO MANY NEW FEATURES OMF why am i yelling

--- src/kappa/Kappa.js
+++ src/kappa/Kappa.js
@@ -1,17 +1,50 @@
 module.exports = Kappa;
+const fs = require('fs');
 
-function Kappa(n) {
+function Kappa(n, t) {
+  if(["Kappa", "KappaPride", "KappaRoss", "KappaClaus", "KappaWealth"].indexOf(t)) {
+    this.type = t;
+  } else {
+    console.error(`not a type : ${t} is not a type`);
+  }
   this.name = n;
   this.age = 0;
-  this.type = 'default';
-  this.timeTypeSet = 0;
-  this.picture = "http://i3.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large";
+  this.photo = "http://i3.kym-cdn.com/photos/images/newsfeed/000/925/494/218.png_large";
   this.types = ["Kappa", "KappaPride", "KappaRoss", "KappaClaus", "KappaWealth"];
-  this.maxAvailableType = 0;
+  this.idCard = {
+    "Name": this.name,
+    "Type": this.type,
+    "Photo": this.photo
+  };
 }
 
 Kappa.prototype.setType = function (typeSelection) {
-  if(typeSelection >= maxAvailableType) {
-    return `You can't do that! You must be level ${typeSelection} or more to select ${types[typeSelection]}`;
-  }
+  this.type == typeSelection;
 };
+
+Kappa.prototype.getTypes = function (joint) {
+  return this.types.join(joint);
+};
+
+Kappa.prototype.getType = function () {
+  return this.type;
+};
+
+Kappa.prototype.exportDataToFile = function (file) {
+  fs.writeFile(file, this, 'utf8', (err) => {
+    if (err) throw err;
+  })
+};
+
+Kappa.prototype.exportData = function (file) {
+  return this;
+};
+
+
+
+function prepareExport() {
+  let data = {
+    "name":this.name,"age":this.age,"photo":this.photo,"type":this.type,"age":this.age
+  }
+  return data;
+}
--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 const myKappa = require('./src/index.js');
 
-var kappa = new myKappa.Kappa("little kappy");
+var kappa = new myKappa.Kappa("little kappy", "Kappa");
+
+
+console.log(kappa.getTypes(", "));
+console.log(kappa.getType());
+console.log(kappa.idCard);
+console.log(kappa.exportData())
+kappa.exportDataToFile('./data.json')
